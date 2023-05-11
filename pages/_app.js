48cdca6b1b0c1ca36a0d5e98a4a0f21a6dec055b
ShahzodTuraev <turayevshahzodbek@gmail.com>
2023-05-12 03:46:33 +0900

    google analytics fixed

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,24 @@
 import '@/styles/globals.css'
 import { AuthUserProvider } from '@/context/authUserContext'
-
+import Script from 'next/script'
 export default function App({ Component, pageProps }) {
-  return <AuthUserProvider><Component {...pageProps} /></AuthUserProvider> 
+  return(
+    <>
+     <Script 
+        strategy='lazyOnload'
+        src={`https://www.googletagmanager.com/gtag/js?id=G-3C6ZYQKWLX`}
+      />
+      <Script strategy='lazyOnload'>
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-3C6ZYQKWLX', {
+            page_path: window.location.pathname,
+          });
+        `}
+      </Script>
+      <AuthUserProvider><Component {...pageProps} /></AuthUserProvider> 
+    </>
+  ) 
 }
