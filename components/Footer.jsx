6e22204a67c13685b1ddd806e6521d2b851071d7
shon <turayevshahzodbek@gmail.com>
2024-02-14 01:34:14 +0900

    fix: modify footer link name and address

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,15 +1,16 @@
-import React from 'react'
+import React from "react";
 
 const Footer = () => {
   return (
     <>
-      <p className='footer'>
-        "Tesla Clone 2023" created by <b>Shahzod Turaev</b>.
-        Creator's portfolio: <a target='_blank' href="https://portfolio-sht.vercel.app">portfolio-sht.vercel.app</a> 
+      <p className="footer">
+        "Tesla Clone 2023" created by <b>Shahzod Turaev</b>. Creator's page:{" "}
+        <a target="_blank" href="https://github.com/ShahzodTuraev">
+          github.com/ShahzodTuraev
+        </a>
       </p>
-      
     </>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
